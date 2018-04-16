9ab5892d9353206fe87aac9a2611ded469a64e8e
cxwcfea <cxwcfea@163.com>
2018-04-16 15:34:42 +0800

    update

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -2,10 +2,12 @@
 
 import os
 
-from flask import Flask, jsonify
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
+
 
 def create_app(script_info=None):
   app = Flask(__name__)
